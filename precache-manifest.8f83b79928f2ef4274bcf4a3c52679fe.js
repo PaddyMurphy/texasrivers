2675fdf695192d7cbe51f7a20886e28d70feb537
plewis <plewis@indeed.com>
2019-07-08 14:49:46 -0500

    Updates

---
+++ precache-manifest.8f83b79928f2ef4274bcf4a3c52679fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5580e27cc0240113edb2c93eeb0d6495",
+    "url": "/texasrivers/index.html"
+  },
+  {
+    "revision": "ed0e62d127e5bc50d5e6",
+    "url": "/texasrivers/static/css/main.0782d5a8.chunk.css"
+  },
+  {
+    "revision": "f1f3adfb594a6b04aa9f",
+    "url": "/texasrivers/static/js/2.bda9a5d6.chunk.js"
+  },
+  {
+    "revision": "ed0e62d127e5bc50d5e6",
+    "url": "/texasrivers/static/js/main.838afa4d.chunk.js"
+  },
+  {
+    "revision": "a88794250ff5af6a8f45",
+    "url": "/texasrivers/static/js/runtime~main.3879094e.js"
+  }
+]);
